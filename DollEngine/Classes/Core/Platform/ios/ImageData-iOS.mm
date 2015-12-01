534208c9d82cf1206c09c05efd5bdc10f68b9eea
DollStudio <yhtm123@qq.com>
2015-12-01 23:35:51 +0800

    update

--- DollEngine/Classes/Core/Platform/ios/ImageData-iOS.mm
+++ DollEngine/Classes/Core/Platform/ios/ImageData-iOS.mm
@@ -9,6 +9,9 @@
 #import <UIKit/UIKit.h>
 #include "PictureData.h"
 #include "Storages.h"
+#include "System.h"
+#include <ft2build.h>
+#include FT_FREETYPE_H
 
 DE_BEGIN
 
@@ -54,6 +57,71 @@ bool PictureData::loadFromFile(const DE::String &fullPath)
         return true;
     }
     return false;
+}
+
+PictureData* PictureData::addText(const String& text,const String& fontName,int fontSize,FontData* fd)
+{
+    if (text.empty()) {
+        return NULL;
+    }
+    FT_Face face = (FT_Face)System::GetInstance()->getFont(fontName);
+    
+    unsigned short charcode = text[0];
+    FT_UInt graphIdx = FT_Get_Char_Index(face, (FT_ULong)charcode);
+    if (graphIdx == 0)
+    {
+        face = (FT_Face)System::GetInstance()->getFont(System::GetInstance()->getDefFontName());
+        graphIdx = FT_Get_Char_Index(face, charcode);
+    }
+    FT_Error ft_err = FT_Set_Char_Size(face,
+                                       fontSize*64,
+                                       fontSize*64,
+                                       72,72);
+    if(ft_err) {
+        return NULL;
+    }
+    ft_err = FT_Load_Glyph(face, graphIdx, FT_LOAD_DEFAULT);
+    if(ft_err) {
+        return NULL;
+    }
+    if (face->glyph->format != FT_GLYPH_FORMAT_BITMAP)
+    {
+        ft_err = FT_Render_Glyph(face->glyph,FT_RENDER_MODE_NORMAL);
+        if(ft_err) {
+            return NULL;
+        }
+    }
+    
+    int width = face->glyph->bitmap.width;
+    int height = face->glyph->bitmap.rows;
+    
+    Rect rect;
+    rect.width = width;
+    rect.height = height;
+    
+    IOData* iodata = new IOData();
+    iodata->initWithSize(width*height*4);
+    for (int x=0; x<width; ++x) {
+        for (int y=0; y<height; ++y) {
+            int idx = y*width*4 + x*4;
+            unsigned char* color = face->glyph->bitmap.buffer + y*width + x;
+            memset(iodata->getBuffer() + idx, *color, 4);
+        }
+    }
+    
+    PictureData* image = new PictureData();
+    image->setData(iodata);
+    image->setWidth(width);
+    image->setHeight(height);
+    
+    fd->advance = (int)(face->glyph->metrics.horiAdvance/64);
+    fd->bearingX = (int)(face->glyph->metrics.horiBearingX/64);
+    fd->bearingY = (int)(face->glyph->metrics.horiBearingY/64);
+    fd->yMin = (int)(face->bbox.yMin/64);
+    fd->yMax = (int)(face->bbox.yMax/64);
+    fd->xMin = (int)(face->bbox.xMin/64);
+    fd->xMax = (int)(face->bbox.xMax/64);
+    return image;
 }
 
 DE_END
